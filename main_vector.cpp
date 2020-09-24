ae1da3dfb3a7901713beaf8e7bfd4aad3d851a0d
dovmar <71687301+dovmar@users.noreply.github.com>
2020-09-24 18:37:11 +0300

    Sutvarkyta programos versija su vektoriais

--- main_vector.cpp
+++ main_vector.cpp
@@ -1,14 +1,15 @@
-﻿
+
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <random>
 
 using std::cout;
 using std::cin;
 using std::string;
-using std:vect;
+using std::vector;
 using std::endl;
 using std::setfill;
 using std::left;
@@ -31,56 +32,52 @@ void teisingasIvedimas() {
     cout << "Iveskite sveikaji skaiciu: ";
 }
 
-
-void ivedamiND(duomuo A[],int i, int m) {
-    // Leidzia ivesti namu darbu pazymius rankiniu budu 
+void ivedamiND(vector<duomuo> &A, int i, int m) {
+    // Leidzia ivesti namu darbu pazymius rankiniu budu
+    int input;
     cout << "Iveskite studento namu darbu pazymius: \n";
     for (int j = 0; j < m; j++) {
-        while (!(cin >> A[i].nd[j])) {
+        while (!(cin >> input)) {
             teisingasIvedimas();
         }
+        A[i].nd.push_back(input);
     }
 }
 
 
-void atsitiktiniaiND(duomuo A[], int i, int m) {
+void atsitiktiniaiND(vector<duomuo> &A, int i, int m) {
     // Sugeneruoja atsitiktinius namu darbu pazymius 
     for (int j = 0; j < m; j++) {
-        A[i].nd[j] = (rand() % 10) + 1;
+        A[i].nd.push_back((rand() % 10) + 1);
     }
 }
 
 
-void naudojantMediana(duomuo A[], int m, int n) {
+void naudojantMediana(vector<duomuo> &A, int i, int m) {
     // Apskaiciuoja galutini pazymi naudojant namu darbu mediana
-    for (int i = 0; i < n; i++) {
-        if (m == 1) A[i].paz = A[i].nd[0];
+    if (m == 1) A[i].paz = A[i].nd[0];
+    else {
+        sort(A[i].nd.begin(),A[i].nd.end());
+        if (m % 2 != 0) {
+            A[i].paz = A[i].nd[m / 2];
+        }
         else {
-            sort(A[i].nd, A[i].nd + m);
-            if (m % 2 != 0) {
-                A[i].paz = A[i].nd[m / 2];
-            }
-            else {
-                A[i].paz = A[i].nd[m / 2] + A[i].nd[m / 2 - 1] / 2;
-            }
+            A[i].paz = (A[i].nd[m / 2] + A[i].nd[m / 2 - 1]) / 2;
         }
-        A[i].paz = A[i].paz * 0.4 + A[i].egz * 0.6;
     }
+    A[i].paz = A[i].paz * 0.4 + A[i].egz * 0.6;
 }
 
-
-void naudojantVidurki(duomuo A[], int m, int n) {
+void naudojantVidurki(vector<duomuo> &A, int i, int m) {
     // Apskaiciuoja galutini pazymi naudojant namu darbu vidurki
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            A[i].paz = A[i].paz + A[i].nd[j];
-        }
-        A[i].paz = A[i].paz * 0.4 / m + A[i].egz * 0.6;
+    for (int j = 0; j < m; j++) {
+        A[i].paz = A[i].paz + A[i].nd[j];
     }
+    A[i].paz = A[i].paz * 0.4 / m + A[i].egz * 0.6;
 }
 
 
-void isvedimas(duomuo A[], int n, string tipas) {
+void isvedimas(vector<duomuo> A, int n, string tipas) {
     // Isveda galutinius pazymius*/
     cout << setw(15) << left << "Pavarde";
     cout << setw(15) << left << "Vardas";
@@ -94,8 +91,8 @@ void isvedimas(duomuo A[], int n, string tipas) {
 
 int main()
 {
-    duomuo *Stud;
-    int n,m;
+    int n, m;
+    vector <duomuo> Stud;
     cout << "Kiek studentu norite ivesti? ";
     while (!(cin >> n)) {
         teisingasIvedimas();
@@ -104,11 +101,10 @@ int main()
     while (!(cin >> m)) {
         teisingasIvedimas();
     }
-    vector <duomuo> Stud;
     for (int i = 0; i < n; i++) {
         Stud.push_back(duomuo());
         cout << "Iveskite studento varda ir pavarde: \n";
-        cin >> Stud[i].vardas
+        cin >> Stud[i].vardas;
         cout << "Iveskite studento pavarde: \n";
         cin >> Stud[i].pav;
         /* Galima arba ivesti pazymius rankiniu budu naudojant ivedamiND()
@@ -120,8 +116,8 @@ int main()
         }
         /* Galima apskaiciuoti galutini pazymi naudojant namu darbu vidurki su naudojantVidurki()
         arba naudojant mediana su naudojantMediana() */
-        if (m > 0) naudojantMediana(Stud, m, n);
-        else Stud[i].paz =  Stud[i].egz;
+        if (m > 0) naudojantMediana(Stud, i, m);
+        else Stud[i].paz = Stud[i].egz;
     }
     // Paskutiniam funkcijos argumentui naudoti pasirinkto pazymio skaiciavimo tipo pavadinima
     isvedimas(Stud, n, "(Med.)");
